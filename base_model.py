bbc41675173b47bb40a4de31bf27a7a5dbc40ea2
Alan Anderson <alanpurple@gmail.com>
2018-05-11 16:53:12 +0900

    init2

--- base_model.py
+++ base_model.py
@@ -18,22 +18,21 @@ def base_model(features, labels, mode, params):
 
     retrieved_category=tf.gather(category_list,retrieved)
     retrieved_emb_with_cate=tf.concat([
-        tf.nn.embedding_lookup(item_emb_w,retrieved),   # [1,H/2]
-        tf.nn.embedding_lookup(cate_emb_w, retrieved_category) # [1,H/2]
-        ],1)    # [1,H]
+        tf.nn.embedding_lookup(item_emb_w,retrieved),   # [B,H/2]
+        tf.nn.embedding_lookup(cate_emb_w, retrieved_category) # [B,H/2]
+        ],1)    # [B,H]
     retrieved_b= tf.gather(item_b,retrieved)
 
     history_cat=tf.gather(category_list,user_history)
     print(tf.shape(history_cat))
     history_emb=tf.concat([
-        tf.nn.embedding_lookup(item_emb_w,user_history), # [T, H/2]
-        tf.nn.embedding_lookup(cate_emb_w,history_cat)   # [T, H/2]
-    ],2) # [T,H]
+        tf.nn.embedding_lookup(item_emb_w,user_history), # [B,T, H/2]
+        tf.nn.embedding_lookup(cate_emb_w,history_cat)   # [B,T, H/2]
+    ],2) # [B,T,H]
 
-    hist_sum=tf.reduce_sum(history_emb,1)   # [1,H]
+    hist_sum=tf.reduce_sum(history_emb,1)   # [B,H]
     hist_mean=tf.div(hist_sum,tf.cast(tf.tile(tf.expand_dims(hist_len,1),[1,hidden_len]),tf.float32))
 
-    print(tf.shape(hist_mean))
     training = mode==tf.estimator.ModeKeys.TRAIN
     hist_mean=tf.layers.batch_normalization(hist_mean,training=training)
     hist_mean=tf.layers.dense(hist_mean,hidden_len)
@@ -64,19 +63,20 @@ def base_model(features, labels, mode, params):
         }
         return tf.estimator.EstimatorSpec(mode,loss=loss,eval_metric_ops=eval_metric_ops)
 
-    hist_emb_all = tf.tile(tf.expand_dims(hist_mean,0),[item_len,1]) # [I,H]
+    hist_emb_all = tf.tile(tf.expand_dims(hist_mean,1),[-1,item_len,1]) # [B,I,H]
 
     all_product_emb=tf.concat([
-        item_emb_w,
-        tf.nn.embedding_lookup(cate_emb_w,category_list)
+        item_emb_w, # [I,H/2]
+        tf.nn.embedding_lookup(cate_emb_w,category_list)    #[I,H/2]
         ],1) # [I,H]
-    hist_emb_all=tf.concat([hist_emb_all,all_product_emb],1)    # [I,2H]
+    all_product_emb=tf.tile(all_product_emb,[tf.shape(hist_emb_all)[0],1,1])    #[B,I,H]
+    hist_emb_all=tf.concat([hist_emb_all,all_product_emb],2)    # [B,I,2H]
     hist_emb_all=tf.layers.batch_normalization(hist_emb_all,name='fc',reuse=True)
     fc_all1=tf.layers.dense(hist_emb_all,80,tf.nn.sigmoid,name='fc1',reuse=True)
     fc_all2=tf.layers.dense(fc_all1,40,tf.nn.sigmoid,name='fc2',reuse=True)
-    fc_all3=tf.layers.dense(fc_all2,1,name='fc3',reuse=True) # [I,1]
-    fc_all3=tf.reshape(fc_all3,[-1])    #[I]
+    fc_all3=tf.layers.dense(fc_all2,1,name='fc3',reuse=True) # [B,I,1]
+    fc_all3=tf.reshape(fc_all3,[-1,item_len])    #[B,I]
 
-    logits_all=tf.sigmoid(item_b,fc_all3)
+    logits_all=tf.sigmoid(item_b+fc_all3)
 
     return tf.estimator.EstimatorSpec(mode,logits_all)
