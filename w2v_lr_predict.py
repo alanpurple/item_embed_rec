158545979eba82e3fe48c3cb9ae9826ced3f2db4
Alan Anderson <alanpurple@gmail.com>
2018-06-20 10:29:42 +0900

    update user profile generation code

--- w2v_lr_predict.py
+++ w2v_lr_predict.py
@@ -1,39 +1,45 @@
 from sklearn.externals import joblib
 from mongoengine import connect
 from mongoengine.errors import DoesNotExist
-import statistics
+import pandas as pd
 
 from models import DealW2v
 from models import PosData
 from models import WepickDeal
 
+HISTORY_FROM='04-01'
+HISTORY_TO='04-10'
+
 connect('wepickw2v',host='mongodb://localhost')
 
-#wepickdata=PosData.objects(TransDate__gte='2018-04-01 00',TransDate__lte='2018-04-10 23',WepickRank__gte=20,WepickRank__lte=55).aggregate(
-#    *[{'$group':{'_id':'$UserId','docs':{'$push':'$$ROOT'}}}],allowDiskUse=True)
+wepickdata=PosData.objects(TransDate__gte='2018-'+HISTORY_FROM+' 00',TransDate__lte='2018-'+HISTORY_TO+' 23',WepickRank__gte=20,WepickRank__lte=55).aggregate(
+    *[{'$group':{'_id':'$UserId','docs':{'$push':'$$ROOT'}}}],allowDiskUse=True)
 # in case cursornotfounderrror caused by very long sampling times
-#wepickdata=list(wepickdata)
-#data=[]
+wepickdata=list(wepickdata)
+feature_columns=['user_id']
+for i in range(100):
+    feature_columns.append('feature_'+str(i+1))
+data=[feature_columns]
 
-userstat=PosData.objects(TransDate__gte='2018-04-01 00',TransDate__lte='2018-04-10 23',WepickRank__gte=20,WepickRank__lte=55).aggregate(
-    *[{'$group':{'_id':'$UserId','count':{'$sum':1}}}],allowDiskUse=True)
+print('Number of Users: ',len(wepickdata))
 
-len_list=[elem['count'] for elem in userstat]
+for elem in wepickdata:
+    if len(elem['docs'])>20 and len(elem['docs'])<40:
+        for doc in elem['docs']:
+            hist_sum=[0]*100
+            result=DealW2v.objects(pk=doc['DealId']).first()
+            if result != None:
+                temp=result.vectorizedWords['values']
+                if len(temp)==100:
+                    if temp[0]!=0 and temp[1]!=0 and temp[2]!=0:
+                        hist_sum=[sum(x) for x in zip(hist_sum,temp)]
+        if hist_sum[0]!=0 and hist_sum[1]!=0 and hist_sum[2]!=0:
+            data.append([elem['_id']]+hist_sum)
 
-print('mean: ',statistics.mean(len_list))
-print('harmonic mean: ',statistics.harmonic_mean(len_list))
-print('min: ',min(len_list))
-print('max: ',max(len_list))
+print('resulting number of users: ',len(data)-1)
 
-long_len_list=[elem for elem in len_list if elem>20]
+profile_df=pd.DataFrame(data)
+profile_df.to_csv('profile_'+HISTORY_FROM+'_'+HISTORY_TO+'.csv')
 
-print(len(long_len_list))
-print(long_len_list[:10])
-
-print('mean: ',statistics.mean(long_len_list))
-print('harmonic mean: ',statistics.harmonic_mean(long_len_list))
-print('min: ',min(long_len_list))
-print('max: ',max(long_len_list))
-
-#print('Number of Users: ')
-#print(len(wepickdata))
+scaler=joblib.load('scaler.pkl')
+wplr=joblib.load('wplr.pkl')
