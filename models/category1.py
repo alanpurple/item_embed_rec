44da1440478bf1f8d6931b69310260906e51bbfd
Alan Anderson <alanpurple@gmail.com>
2018-08-20 15:26:30 +0900

    add meta

--- models/category1.py
+++ models/category1.py
@@ -1,5 +1,6 @@
 from mongoengine import Document,StringField,IntField
 
 class Category1(Document):
+    meta={'collection':'category1'}
     primary=IntField(primary_key=True)
     name=StringField()
--- models/category2.py
+++ models/category2.py
@@ -1,5 +1,6 @@
 from mongoengine import Document,StringField,IntField
 
 class Category2(Document):
+    meta={'collection':'category2'}
     primary=IntField(primary_key=True)
     name=StringField()
